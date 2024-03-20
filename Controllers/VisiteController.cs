949931579f39da6cc71f6c6d66b1211fa7091580
DaveGio11 <145689622+DaveGio11@users.noreply.github.com>
2024-03-20 12:50:27 +0100

    committ-cronistoria

---
+++ Controllers/VisiteController.cs
@@ -0,0 +1,148 @@
+﻿using BW_Clinica_Veterinaria.Models;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace BW_Clinica_Veterinaria.Controllers
+{
+    public class VisiteController : Controller
+    {
+        private ModelDBContext db = new ModelDBContext();
+
+        // GET: Visite
+        public ActionResult Index()
+        {
+            var visite = db.Visite.Include(v => v.Animali);
+            return View(visite.ToList());
+        }
+
+        // GET: Visite/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visite visite = db.Visite.Find(id);
+            if (visite == null)
+            {
+                return HttpNotFound();
+            }
+            return View(visite);
+        }
+
+        //Cronistoria
+
+        public ActionResult Cronistoria(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var visite = db.Visite.Where(v => v.AnimaleID == id).OrderByDescending(v => v.Data).ToList();
+            if (visite == null)
+            {
+                return HttpNotFound();
+            }
+            return View(visite);
+
+        }
+
+        // GET: Visite/Create
+        public ActionResult Create()
+        {
+            ViewBag.AnimaleID = new SelectList(db.Animali, "AnimaleID", "Nome");
+            return View();
+        }
+
+        // POST: Visite/Create
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "VisitaID,AnimaleID,Data,Esame,Cura")] Visite visite)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Visite.Add(visite);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AnimaleID = new SelectList(db.Animali, "AnimaleID", "Nome", visite.AnimaleID);
+            return View(visite);
+        }
+
+        // GET: Visite/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visite visite = db.Visite.Find(id);
+            if (visite == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AnimaleID = new SelectList(db.Animali, "AnimaleID", "Nome", visite.AnimaleID);
+            return View(visite);
+        }
+
+        // POST: Visite/Edit/5
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "VisitaID,AnimaleID,Data,Esame,Cura")] Visite visite)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(visite).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AnimaleID = new SelectList(db.Animali, "AnimaleID", "Nome", visite.AnimaleID);
+            return View(visite);
+        }
+
+        // GET: Visite/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visite visite = db.Visite.Find(id);
+            if (visite == null)
+            {
+                return HttpNotFound();
+            }
+            return View(visite);
+        }
+
+        // POST: Visite/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Visite visite = db.Visite.Find(id);
+            db.Visite.Remove(visite);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
